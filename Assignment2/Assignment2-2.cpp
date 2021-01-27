8b73bc378f1416fa23e242d11d6a71fbe6b1cf52
Michael Muetzenberg <michael.muetzenberg@gmail.com>
2021-01-27 17:32:55 +0000

    Assignment 2-3

---
+++ Assignment2/Assignment2-2.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+
+    char c;
+
+    c = 77;
+    cout << c;
+
+    c = 105;
+    cout << c;
+
+    c = 99;
+    cout << c;
+
+    c = 104;
+    cout << c;
+
+    c = 97;
+    cout << c;
+
+    c = 101;
+    cout << c;
+
+    c = 108;
+    cout << c << "\n";
+
+    
+    
+
+}
---
+++ Assignment2/Assignment2-3.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+  double regular_hours;
+  double regular_wages, overtime_wages;
+  double base_pay_rate=18.25, overtime_pay_rate =27.78;
+  double regular_hours=40, overtime_hours;
+  double total_wages;
+
+  regular_wages = regular_hours * base_pay_rate;
+	overtime_hours = work_hours - regular_hours;
+	overtime_wages = overtime_hours * overtime_pay_rate;
+	total_wages = regular_wages + overtime_wages;
+
+	cout << "Regular wages " << regular_wages << endl;
+	cout << "Overtime wages " << overtime_wages << endl;
+	cout << "Total wages " << total_wages << endl;
+
+}
+
