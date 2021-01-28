f5cde1de5265d2d778e11a8a44d433acbde29dcf
Michael Muetzenberg <michael.muetzenberg@gmail.com>
2021-01-28 19:54:26 +0000

    Assignment 2-4

---
+++ Assignment2/c1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  double meal_price = 88.67;
+  float meal_tax_percent = 1.0675;
+  double meal_tip_percent = 0.2;
+
+  double meal_tax = meal_price * meal_tax_percent;
+  double meal_tip = meal_tax * meal_tip_percent;
+  double meal_total = meal_tip + meal_tip + meal_tax;
+
+  cout << "Meal cost: $" << meal_price << endl;
+  cout << "Tax amount:  $" << meal_tax << endl;
+  cout << "Tip amount: $" << meal_tip << endl;
+  cout << "Meal total: $" <<< meal_total << endl;
+
+  return 0;
+}
