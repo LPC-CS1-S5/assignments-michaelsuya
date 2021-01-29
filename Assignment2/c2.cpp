705558dffed132df5921623ca70c4127510bd0a9
Michael Muetzenberg <michael.muetzenberg@gmail.com>
2021-01-29 01:26:06 +0000

    Challenge 2

---
+++ Assignment2/c2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n1 = 28;
+    int n2 = 32;
+    int n3 = 37;
+    int n4 = 24;
+    int n5 = 33;
+    int sum;
+    float average;
+
+    sum = n1 + n2 + n3 + n4 + n5;
+    average = sum/5;
+
+    cout << average;
+
+
+
+  
+
+
+
+}
