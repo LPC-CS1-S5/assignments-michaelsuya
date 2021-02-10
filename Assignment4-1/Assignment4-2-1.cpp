fb2730f7d51d97c5a1156c947cc60e7abed9efcd
Michael Muetzenberg <michael.muetzenberg@gmail.com>
2021-02-10 19:08:29 +0000

    Lab 4-2

--- Assignment4-1/Assignment4-2-1.cpp
+++ Assignment4-1/Assignment4-2-1.cpp
@@ -3,7 +3,6 @@
 using namespace std;
 
 int main() {
-
   double price = 99;
   int numsold;
   double originalprice;
@@ -16,43 +15,29 @@ int main() {
 
   cout << setprecision(2) << fixed;
 
-  if (numsold < 0) {
-    cout << "Input Error!\n";
+  if (numsold < 10) {
+    cout << "No discounts available!\n";
   }
 
-  if (numsold >= 10 && numsold <= 19) {
+  else if (numsold <= 19) {
     discount = .2;
-    originalprice = numsold * price;
-    discountamount = discount * originalprice;
-    totalprice = originalprice - discountamount;
-    cout << "The original price is $" << originalprice << "\n";
-    cout << "The discounted price is $" << totalprice << "\n";
   }
-  else if (numsold >= 20 && numsold <= 49){
+  else if (numsold <= 49){
     discount = .3;
-    originalprice = numsold * price;
-    discountamount = discount * originalprice;
-    totalprice = originalprice - discountamount;
-    cout << "The original price is $" << originalprice << "\n";
-    cout << "The discounted price is $" << totalprice << "\n";
   }
-  else if (numsold >= 50 && numsold <= 99) {
+  else if (numsold <= 99) {
     discount = .4;
-    originalprice = numsold * price;
-    discountamount = discount * originalprice;
-    totalprice = originalprice - discountamount;
-    cout << "The original price is $" << originalprice << "\n";
-    cout << "The discounted price is $" << totalprice << "\n";
   }
   else {
     if (numsold >= 100) {
       discount = .5;
+  	}
+  }
       originalprice = numsold * price;
       discountamount = discount * originalprice;
       totalprice = originalprice - discountamount;
       cout << "The original price is $" << originalprice << "\n";
+      cout << "Your discount is $" << discountamount << "\n";
       cout << "The discounted price is $" << totalprice << "\n";
-    }
-  }
 
   }
