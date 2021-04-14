d06e39640b5f27b9679254129c525d7c1991f22b
Michael Muetzenberg <>
2021-04-14 03:53:06 +0000

    Quiz 6-2 Q1

---
+++ Quiz6-2/Q1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+void swapTwoValues(int, int);
+void swapTwoValues(float, float);
+
+int main() {
+
+int n1, n2;
+float f1, f2;
+
+cout << "User inputs: ";
+cin >> n1 >> n2;
+cout << "User float inputs: ";
+cin >> f1 >> f2;
+
+swapTwoValues(n1, n2);
+swapTwoValues(f1, f2);
+
+}
+
+void swapTwoValues(int n1, int n2) {
+  cout << "Your two integers are " << n1 << " and " << n2 << endl;
+}
+void swapTwoValues(float f1, float f2) {
+  cout << "Your two float values are " << f1 << " and " << f2 << endl;
+}
---
+++ Quiz6-2/Q3.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+void getRandnum(int, int, int);
+void findMin(int, int, int);
+void printResult(int, int, int, int);
+
+int main() {
+  int n1, n2, n3, min;
+
+  srand(time(0));
+
+  getRandnum(n1, n2, n3);
+  findMin(n1, n2, n3);
+  printResult(n1, n2, n3, min);
+}
+
+void getRandnum(int &n1, int &n2, int &n3) {
+  int rdnum1 = rand() % 20;
+  int rdnum2 = rand() % 20;
+  int rdnum3 = rand() % 20;
+  int& n1 = rdnum1;
+  int& n2 = rdnum2;
+  int& n3 = rdnum3;
+  return rdnum;
+}
+
+void findMin(int n1, int n2, int n3) {
+   for (int i = 0; i < 3; i++) {
+      if (i == 0) {
+          min = n1;
+      }
+
+      else {
+        if (n1 < min) {
+          min = rdnum;
+
+      }
+    }
+  }    
+}
+
+void printResult(int n1, int n2, int n3, int min) {
+  cout << "N1: " << n1;
+  cout << "N2: " << n2;
+  cout << "N3: " << n3;
+  cout << "Min: " << min;
+}
